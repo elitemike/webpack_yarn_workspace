d49a178a01750a90425eb8f71e08384443e62ee9
Mike Stankovich <mike.stankovich@outlook.com>
2023-06-26 09:56:44 -0400

    cleanup

--- applications/demo-app/src/main.ts
+++ applications/demo-app/src/main.ts
@@ -2,20 +2,11 @@ import { bootstrap } from 'aurelia-bootstrapper';
 import { Aurelia, PLATFORM, LogManager } from 'aurelia-framework';
 import { App } from './app';
 import { ConfigBuilder } from 'aurelia-syncfusion-ej2-bridge';
-import { registerLicense } from '@syncfusion/ej2/base';
-import { ConsoleAppender } from "aurelia-logging-console"
-
 
 bootstrap(async (aurelia: Aurelia) => {
-
-
   const logger = LogManager.getLogger("bootstrapper");
 
-
-
   aurelia.use.standardConfiguration().developmentLogging();
-
-
 
   aurelia.use.plugin(PLATFORM.moduleName('@ui/helpers'));
   aurelia.use.plugin(PLATFORM.moduleName("aurelia-syncfusion-ej2-bridge"), (configBuilder: ConfigBuilder) => configBuilder.useDefaults());
